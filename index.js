0404514fc0bab9e7827f1d3963d65c4bc245fa11
Tez <38469636+TezSmith@users.noreply.github.com>
2019-03-27 12:53:26 -0400

    New Notes on JS Class Syntax

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// document.addEventListener('DOMContentLoaded', init)
-
-// function init() {
-//    console.log("Hello World!")
-// }
-
 // const circle = {
 //   radius: 1,
 //   location: {
@@ -16,68 +10,41 @@
 // }
 
 //Factory Function
-// function createCircle(radius) {
-//   return {
-//     radius,
-//     draw: function() {
-//       console.log('draw')
-//     }
-//  }
-// }
-// const circle = createCircle(1)
-//
-// //Constuctor Function
-// function Circle(radius) {
-//   this.radius = radius
-//   this.draw = function() {
-//     console.log('draw')
-//   }
-// }
-//
-// const another = new Circle(1)
+function createCircle(radius) {
+  return {
+    radius,
+    draw: function() {
+      console.log('draw')
+    }
+ }
+}
+const circle = createCircle(1)
 
-// //ES6 Classes
-// class Circle {
-//   constructor(radius){
-//     this.radius = radius
-//   }
-//
-//   draw() {
-//     console.log('Tez')
-//   }
-// }
-//
-// const tez = new Circle(1)
-// tez.draw()
-
-
-//@ 59 mins let's try to make a Object Oriented Stopwatch
-class Stopwatch {
-  duration = 0
-
-  get duration(){
-    return duration
+//Constuctor Function
+function Circle(radius) {
+  this.radius = radius
+  this.draw = function() {
+    console.log('draw')
   }
-
-  increase = () => {
-  }
-
-
-  start() {
-    console.log("Start")
-  }
-
-  stop(){
-    console.log("Stop")
-  }
-
 }
 
-let sw = new Stopwatch()
-console.log("This the stopwatch", sw)
+const another = new Circle(1)
 
+//ES6 Classes
+class Circle {
+  constructor(radius){
+    this.radius = radius
+  }
 
+  draw() {
+    console.log('Tez')
+  }
+}
 
+const tez = new Circle(1)
+tez.draw()
 
+/* ES6 Classes don't allow you to define variables within the class. 
+Also, to use getter and setter methods, you'll have to use keywords get & set beside 
+the respective functions.*/
 
-////
